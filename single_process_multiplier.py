32f3b24f6d417425205bd3fd7172222040a5992d
eranga herath <erangaeb@gmail.com>
2015-10-15 22:32:23 +0530

    single core done

--- single_process_multiplier.py
+++ single_process_multiplier.py
@@ -56,6 +56,49 @@ def multiply_matrix(X, Y):
     print Z
 
 
+def multiply():
+    """
+    Multiply matrix via my own algorithm, Following is the algorithm to matrix
+    multiplication
+
+    X = [
+            [1, 2, 3, 4],
+            [3, 2, 3, 7],
+        ]
+
+    Y = [
+            [1, 2, 3],
+            [5, 6, 7],
+            [2, 2, 7],
+            [5, 6, 9],
+        ]
+
+    Z = [
+            [(1*1 + 2*5 + 3*2 + 4*5), (1*2 + 2*6 + 3*2 + 4*6), --, --],
+            [(3*1 + 2*5 + 3*2 + 7*5), (3*2 + 2*6 + 3*2 + 7*6), --, --]
+        ]
+
+    Args:
+        X: rows of mtrx1
+        Y: whole mtrx2
+
+    Returns:
+        Z: calculated matrix part
+    """
+    Z = [[0] * len(Y[0]) for i in range(len(X))]
+
+    i = 0
+    for a in X:
+        j = 0
+        for b in a:
+            k = 0
+            for c in Y[j]:
+                Z[i][k] = Z[i][k] + (b * c)
+                k = k + 1
+            j = j + 1
+        i = i + 1
+
+
 if __name__ == '__main__':
     """
     Main method here, we have to do
@@ -64,20 +107,3 @@ if __name__ == '__main__':
     """
     init_matrix()
     multiply_matrix(X, Y)
-
-
-'''
-#Z = [[0] * N for i in range(N)]
-Z = [[0] * len(mtrx2[0]) for i in range(len(mtrx1))]
-
-i = 0
-for a in mtrx1:
-    j = 0
-    for b in a:
-        k = 0
-        for c in mtrx2[j]:
-            Z[i][k] = Z[i][k] + (b * c)
-            k = k + 1
-        j = j + 1
-    i = i + 1
-'''
